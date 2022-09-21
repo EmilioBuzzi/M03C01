049682a117210b6a0e84d301fda12edb20cc9c9b
SofiChaves <sofiamagdalenachaves@gmail.com>
2022-09-20 23:31:26 -0300

    Modificación punto 7

--- A.js
+++ A.js
@@ -1,3 +1,4 @@
 console.log("//*******************************************/");
 console.log("Archivo A");
 console.log("//*******************************************/");
+console.log('Estoy modificando')
